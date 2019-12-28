4248c5ba7423b9a29f61c9f95325c2ec67a5c6d1
ats-y <london.rightside@gmail.com>
2019-12-28 16:34:58 +0900

    追加漏れ。

---
+++ BindingSample/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BindingSample.Models;
+using Prism.Mvvm;
+using Xamarin.Forms;
+
+namespace BindingSample.ViewModels
+{
+    public class EmployeeViewModel : BindableBase
+    {
+        public Employee Employee { get; set; }
+
+        public EmployeeViewModel()
+        {
+        }
+
+        public string DisplayName
+        {
+            get => $"{Employee.FamilyName} {Employee.GivenName}";
+        }
+
+        public string DisplaySex
+        {
+            get
+            {
+                switch (Employee.Sex)
+                {
+                    case Employee.ESex.Male:
+                        return "男性";
+                    case Employee.ESex.Female:
+                        return "女性";
+                }
+                return "不明";
+            }
+        }
+
+        public Color SexColor
+        {
+            get
+            {
+                switch (Employee.Sex)
+                {
+                    case Employee.ESex.Male:
+                        return Color.Blue;
+                    case Employee.ESex.Female:
+                        return Color.Pink;
+                }
+                return Color.Black;
+            }
+        }
+    }
+}
