56f05bdab5225f10ed698efc7b11387d163cb390
ats-y <london.rightside@gmail.com>
2020-01-29 21:21:53 +0900

    状態をアイコン表示するStatusIconViewを追加。

---
+++ BindingSample/Views/StatusIconView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using static BindingSample.Models.Safety;
+
+namespace BindingSample.Views
+{
+    public partial class StatusIconView : ContentView
+    {
+        /// <summary>
+        /// 本人状態種別
+        /// </summary>
+        public EStatus Status { get; set; }
+
+        public StatusIconView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            switch (Status)
+            {
+                case EStatus.Safe:
+                    StatusText.Text = "無事";
+                    Frame.BackgroundColor = Color.Aquamarine;
+                    break;
+                case EStatus.Injury:
+                    StatusText.Text = "怪我";
+                    Frame.BackgroundColor = Color.IndianRed;
+                    break;
+                case EStatus.Sick:
+                    StatusText.Text = "病気";
+                    Frame.BackgroundColor = Color.HotPink;
+                    break;
+                default:
+                    StatusText.Text = "不明";
+                    Frame.BackgroundColor = Color.LightSlateGray;
+                    break;
+            }
+        }
+    }
+}
